87d2bde618a3fbdca32fd100b831fe23d44cd120
Rossy1995 <lordross11@googlemail.com>
2017-05-04 11:33:19 +0100

    Added initial activities

--- app/src/main/java/com/example/ross/iotms/MainActivity.java
+++ app/src/main/java/com/example/ross/iotms/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
