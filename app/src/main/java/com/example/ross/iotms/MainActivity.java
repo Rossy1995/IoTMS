b4f42af80ff8c53400f4490e20eb83553633da98
HarryFaulkner <flkn.haz@gmail.com>
2017-05-04 20:32:13 +0100

    Form put into a dialog box instead of new activity

--- app/src/main/java/com/example/ross/iotms/MainActivity.java
+++ app/src/main/java/com/example/ross/iotms/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.ross.iotms;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -23,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    static Context mDialogContext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +43,41 @@ public class MainActivity extends AppCompatActivity {
         //this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDialogContext = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //here
+                /*
                 startActivity(new Intent(MainActivity.this, AddActivity.class));
-                // or:
+
                 startActivity(new Intent(v.getContext(), AddActivity.class));
+                */
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View myView = getLayoutInflater().inflate(R.layout.dialog_add_device, null);
+                final EditText mName = (EditText) myView.findViewById(R.id.EditTextName);
+                final EditText mDescription = (EditText) myView.findViewById(R.id.EditDecription);
+                spinner = (Spinner) myView.findViewById(R.id.TypeSpinner);
+                adapter = ArrayAdapter.createFromResource(mDialogContext, R.array.devicetypelist, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                Button mLogIn = (Button) myView.findViewById(R.id.submitButton);
+
+                mLogIn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!mName.getText().toString().isEmpty() && !mDescription.getText().toString().isEmpty()){
+                            Toast.makeText(MainActivity.this, "Device Added", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                mBuilder.setView(myView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
             }
         });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
